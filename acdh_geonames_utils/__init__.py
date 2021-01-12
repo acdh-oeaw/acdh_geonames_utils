7024ca657af1657462ca12eda4a9e99e4b621f7b
csae8092 <peter.andorfer@oeaw.ac.at>
2021-01-12 11:08:17 +0100

    Bump version: 0.3.0 → 0.3.1

--- acdh_geonames_utils/__init__.py
+++ acdh_geonames_utils/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Peter Andorfer"""
 __email__ = 'peter.andorfer@oeaw.ac.at'
-__version__ = '0.3.0'
+__version__ = '0.3.1'
--- setup.py
+++ setup.py
@@ -55,6 +55,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/acdh-oeaw/acdh_geonames_utils',
-    version='0.3.0',
+    version='0.3.1',
     zip_safe=False,
 )
